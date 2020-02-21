e65735ba606358c1185c7294a2cf729c61d48e8c
Ivan Felipe Asensio <ivan.felipe@qlik.com>
2020-02-21 09:38:11 +0100

    First version

---
+++ EasyBookmarks.js
@@ -0,0 +1,93 @@
+define( [
+	'qlik',
+	'text!./template.ng.html',
+	'text!./dialog-template.ng.html',
+	'text!./delete-template.ng.html',
+	'ng!$q'
+],
+function ( qlik, template, dialogTemplate, deleteTemplate, $q) {
+	'use strict';
+
+	return {
+		support: {
+			export: false,
+			exportData: false,
+			snapshot: false
+		},
+		template: template,
+		controller: ['$scope', 'luiDialog', function ( $scope, luiDialog ) {
+			var app = qlik.currApp();			
+			var myBasicRow = {
+			    "qInfo": {
+			        "qId": "0",
+					"qType": "bookmark"
+			    },
+			    "qMeta": {
+			        "title": "- Select a bookmark -",
+			        "description": "- Select a bookmark -",
+			        "published": true
+			    },
+			    "qData": {
+			        "title": "- Select a bookmark -"					
+			    }
+			}
+			$scope.selectedBookmark = "0";
+			app.getList( 'BookmarkList', function ( items ) {
+				$scope.bookmarks = items.qBookmarkList.qItems;													
+				$scope.bookmarks.push(myBasicRow);				
+			})
+			
+			$scope.alertValue = function(){  	
+
+				var aplyBookmark = $('#repeatSelect').find('option:selected')[0].value;
+			 	app.bookmark.apply(aplyBookmark);				 	
+
+		    } 
+
+		    $scope.defaultSelect = function (){
+		    	$('#repeatSelect  option[value="0"]').prop("selected", true);
+		    }
+
+		    $scope.verifyOwner = function(){  
+		    	
+		    	var vPub = $('#repeatSelect').find('option:selected').attr("name");
+		    	
+		    	if(vPub == 'false'){
+			 		 $('#delId').prop('disabled', false);			 		 
+			 	}else{
+			 		$('#delId').prop('disabled', true);			 		
+			 	}
+		    } 		    
+			
+			$scope.openDialog = function() {				
+				luiDialog.show({
+					template: dialogTemplate,
+					input: {
+						name : $scope.name,
+						description : $scope.description
+					},
+					controller: ['$scope', function( $scope ) {
+						$scope.text = 'Plase, confirm you want to delete the bookmark';
+						$scope.create = function(){  
+							name = $('#name').val();
+							app.bookmark.create(name,$('#description').val()); 							
+				    	} 
+					}]			
+				});				
+			};
+			
+			$scope.deleteDialog = function() {
+				var aplyBookmark = $('#repeatSelect').find('option:selected')[0].value;				
+				luiDialog.show({
+					template: deleteTemplate,					
+					controller: ['$scope', function( $scope ) {
+						$scope.text = 'Plase, confirm you want to delete the bookmark';
+						$scope.delete = function(){  
+							app.bookmark.remove(aplyBookmark); 														
+				    	} 
+					}]
+				});				
+			};
+		}]
+	};
+});
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# EasyBookmarks
- Smart bookmarks managment object for mashups
+# Easy Bookmarks
+Author:Ivan Felipe ivan.felipe@qlik.com
+QlikTech Iberia,s.l.
+
+Qlik Sense Extension for creating/updating a master measure
+
+It's very useful to keep bookmarks features in mashups, avoiding the use of APIs.
+
+This extension is provided 'as is' and may stop working at any time (i.e. when changes are made to the Qlik Sense APIs).
+
+AngularJS
+
+Present Features:
+<UL>
+  <li>Select and apply bookmarks</li>
+  <li>Delete my own bookmarks</li>
+  <li>Create new bookmarks</li>
+  <li>Use a simple combo box to work</li>
+  <li>Clear / bold font for private / public bookmarks</li>
+</UL>
+
+Studying Features:
+<UL>
+  <li>Sharing private bookmarks</li>
+</UL>
+
+![alt tag](https://github.com/iviasensio/Guides/blob/master/EasyBookmarks/EasyBookmarks.png)
